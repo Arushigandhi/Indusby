802fb780e3ea1b7a76217c22f83bacbfc99ae48b
Arushigandhi <75487137+Arushigandhi@users.noreply.github.com>
2021-11-28 15:58:33 +0530

    working on onboarding page

--- src/pages/Onboarding/index.tsx
+++ src/pages/Onboarding/index.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import './Onboarding.css';
+import onboarding from "./onboarding1.png"
+import { IonFab, IonFabButton, IonIcon, IonFabList } from '@ionic/react';
+import {  arrowForwardCircle } from 'ionicons/icons';
+
+
 
 function index() {
     return (
         <div>
-            Onboarding
+            <img src={onboarding} alt=""/>
+            <h1 className="title">What is Indusby?</h1>
+            <h2 className="caption">We will connect you to avenues of industry waste 
+            that you can make use of for your projects, 
+            thus boosting sustainability!</h2>
+            <IonFab vertical="bottom" horizontal="end" slot="fixed" className="next_button" color="primary">
+                <IonFabButton>
+                    <IonIcon icon={arrowForwardCircle} />
+                </IonFabButton>
+            </IonFab>
         </div>
     )
 }
