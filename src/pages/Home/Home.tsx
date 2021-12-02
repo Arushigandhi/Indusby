d74ff39f77c2084a5dd29030ff2f0d4b00defe13
Arushigandhi <75487137+Arushigandhi@users.noreply.github.com>
2021-12-02 12:06:48 +0530

    trying firebase config

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,7 +5,6 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 
-
 const Home: React.FC = () => {
 
   return(
