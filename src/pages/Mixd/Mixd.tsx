033e2fa1182e28f677dd445a7476fa3f70f705ab
Arushigandhi <75487137+Arushigandhi@users.noreply.github.com>
2021-12-03 07:11:15 +0530

    updating

--- src/pages/Mixd/Mixd.tsx
+++ src/pages/Mixd/Mixd.tsx
@@ -6,23 +6,22 @@ import { IonPage, IonContent } from "@ionic/react";
 
 const Mixd: React.FC = () => {
   return (
-    // <IonPage>
-    <IonContent>
+    <IonPage>
+    
       <div>
         <img src={BG} className="mixd_overview" alt="" />
         <h2 className="mixd_overview_text">What is MixD?</h2>
       </div>
       <h1 className="mixd_title">Consult a Professor:</h1>
-      <div className="prof_scroll">
+      <IonContent className="prof_scroll">
         <ProfList profName="Arushi Gandhi" dotColour="#FFC542"></ProfList>
         <ProfList profName="Aryamann Ningombam" dotColour="#3ED598"></ProfList>
         <ProfList profName="Jyothsna Ashok" dotColour="#FF565E"></ProfList>
         <ProfList profName="Dhruva Goyal" dotColour="#FFC542"></ProfList>
         <ProfList profName="Vedika Sharma" dotColour="#3ED598"></ProfList>
         <ProfList profName="Ananya Tewari" dotColour="#FF565E"></ProfList>
-      </div>
     </IonContent>
-    // </IonPage>
+    </IonPage>
   );
 };
 
